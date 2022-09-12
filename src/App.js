001c608999aec484f948b6a92b48ca5d5d36f485
theViren <viren.353.vss@gmail.com>
2022-09-12 10:09:48 +0530

    My Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import MovieList from './MovieList'
+
+export default function App() {
+  return (
+    <div className='app'>
+      <h1 >MoviePedia</h1>
+    < MovieList />
+    </div>
+  ); 
+}
